8734e0b44dbe87f98c478748d22c8d4b580cb274
Ryan Chang <rchang0369@gmail.com>
2020-05-04 17:11:30 -0400

    Added new abstract classes to help with images

---
+++ RuinsOfAlbertrizal/IconedObjectOfAlbertrizal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace RuinsOfAlbertrizal
+{
+    public abstract class IconedObjectOfAlbertrizal : ObjectOfAlbertrizal
+    {
+        public string IconLocation { get; set; }
+
+        protected Bitmap icon = Properties.Resources.error;
+
+        [XmlIgnore]
+        public Bitmap Icon
+        {
+            get
+            {
+                try
+                {
+                    icon = new Bitmap(IconLocation);
+                }
+                catch (Exception)
+                {
+
+                }
+                return icon;
+            }
+        }
+    }
+}
---
+++ RuinsOfAlbertrizal/WorldMapObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace RuinsOfAlbertrizal
+{
+    /// <summary>
+    /// Has both an icon and a in-game image
+    /// </summary>
+    public class WorldMapObject : IconedObjectOfAlbertrizal
+    {
+        public string WorldImgLocation { get; set; }
+
+        protected Bitmap worldImg = Properties.Resources.error;
+
+        [XmlIgnore]
+        public Bitmap WorldImg
+        {
+            get
+            {
+                try
+                {
+                    worldImg = new Bitmap(WorldImgLocation);
+                }
+                catch (Exception)
+                {
+
+                }
+                return worldImg;
+            }
+        }
+    }
+}
