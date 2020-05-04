aca8b2a0b6a1ba54203d7a0ea88c113a6c9a3d6b
Ryan Chang <rchang0369@gmail.com>
2020-05-04 16:42:09 -0400

    Added prompt for consumables

---
+++ RuinsOfAlbertrizal/Editor/CreateConsumablePrompt.xaml.cs
@@ -0,0 +1,58 @@
+﻿using RuinsOfAlbertrizal.Editor.AdderPrompts;
+using RuinsOfAlbertrizal.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RuinsOfAlbertrizal.Editor
+{
+    /// <summary>
+    /// Interaction logic for CreateConsumablePrompt.xaml
+    /// </summary>
+    public partial class CreateConsumablePrompt : EditorInterface
+    {
+        public static Consumable CreatedConsumable { get; set; }
+
+        public CreateConsumablePrompt()
+        {
+            InitializeComponent();
+            UpdateComponent();
+            DataContext = CreatedConsumable;
+        }
+
+        protected override void UpdateComponent()
+        {
+            if (CreatedConsumable == null)
+                CreatedConsumable = new Consumable();
+        }
+
+        protected override void ClearVariable()
+        {
+            CreatedConsumable = new Consumable();
+        }
+
+        protected override void AddRequiredControls()
+        {
+            RequiredControls.Add(ConsumableName);
+            RequiredControls.Add(DropChance);
+        }
+
+        private void EditBuffBtn_Click(object sender, RoutedEventArgs e)
+        {
+            BuffAdderPrompt buffAdderPrompt = new BuffAdderPrompt(CreatedConsumable.Buffs);
+            buffAdderPrompt.Show();
+            CreatedConsumable.Buffs = buffAdderPrompt.TargetBuffs;
+        }
+    }
+}
