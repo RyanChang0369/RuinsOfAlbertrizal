d8fd273db0f9a2fac7002d3fcfae30bd5e7c7d90
Ryan Chang <rchang0369@gmail.com>
2020-05-04 13:00:37 -0400

    Removed deep cloning

--- RuinsOfAlbertrizal/MiscMethods.cs
+++ RuinsOfAlbertrizal/MiscMethods.cs
@@ -53,16 +53,5 @@ namespace RuinsOfAlbertrizal
                 return new BitmapImage();
             }
         }
-
-        public static T DeepClone<T>(this T a)
-        {
-            using (MemoryStream stream = new MemoryStream())
-            {
-                BinaryFormatter formatter = new BinaryFormatter();
-                formatter.Serialize(stream, a);
-                stream.Position = 0;
-                return (T)formatter.Deserialize(stream);
-            }
-        }
     }
 }
