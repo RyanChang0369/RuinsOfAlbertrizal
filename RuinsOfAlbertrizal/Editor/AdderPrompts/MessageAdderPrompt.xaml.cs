82c557bf51bbe216bf6e3424e26ad4cd048e145b
Ryan Chang <rchang0369@gmail.com>
2020-05-06 14:03:44 -0400

    Added windows to select messages.

---
+++ RuinsOfAlbertrizal/Editor/AdderPrompts/MessageAdderPrompt.xaml.cs
@@ -0,0 +1,55 @@
+﻿using RuinsOfAlbertrizal.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RuinsOfAlbertrizal.Editor.AdderPrompts
+{
+    /// <summary>
+    /// Interaction logic for MessageAdderPrompt.xaml
+    /// </summary>
+    public partial class MessageAdderPrompt : Window
+    {
+        public List<Message> Messages = new List<Message>();
+
+        private List<string> Lines = new List<string>();
+
+        private string ObjectName = "";
+
+        public MessageAdderPrompt()
+        {
+            InitializeComponent();
+        }
+
+        public MessageAdderPrompt(string objName)
+        {
+            ObjectName = objName;
+            Header.Content = $"Add Message for {objName}";
+        }
+
+        private void AddMessageBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageSelect messageSelect = new MessageSelect(ObjectName, new Message());
+            messageSelect.ShowDialog();
+            AddedMessagesListBox.Items.Add(messageSelect.GetLines()[0]);
+        }
+
+        private void AddedMessagesListBox_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            int index = AddedMessagesListBox.SelectedIndex;
+
+            AddedMessagesListBox.Items.RemoveAt(index);
+            Messages.RemoveAt(index);
+        }
+    }
+}
---
+++ RuinsOfAlbertrizal/Editor/AdderPrompts/MessageSelect.xaml.cs
@@ -0,0 +1,67 @@
+﻿using RuinsOfAlbertrizal.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RuinsOfAlbertrizal.Editor.AdderPrompts
+{
+    /// <summary>
+    /// Interaction logic for LevelSelect.xaml
+    /// </summary>
+    public partial class MessageSelect : Window
+    {
+        public MessageSelect()
+        {
+            InitializeComponent();
+        }
+
+        public MessageSelect(string objectName, Message message)
+        {
+            InitializeComponent();
+            this.Title = $"Enter a Message for {objectName}";
+
+            try
+            {
+                ValueTextBox.Text = message.ToString();
+            }
+            catch (NullReferenceException)
+            {  }
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        public List<string> GetLines()
+        {
+            string[] delimiter = { "\r\n" };
+
+            try
+            {
+                return ((string)ValueTextBox.Text).Split(delimiter, StringSplitOptions.None).ToList<string>();
+            }
+            catch (NullReferenceException)
+            {
+                List<string> emptyList = new List<string>();
+                emptyList.Add("");
+                return emptyList;
+            }
+        }
+
+        public Message GetMessage()
+        {
+            return new Message(GetLines());
+        }
+    }
+}
