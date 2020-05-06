db87f41da7003e90035a0bbfc0b5713a599ae7e7
Ryan Chang <rchang0369@gmail.com>
2020-05-06 13:23:14 -0400

    Added converter for lists

---
+++ RuinsOfAlbertrizal/Editor/Converter/ListConverter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace RuinsOfAlbertrizal.Editor.Converter
+{
+    public class ListConverter : IValueConverter
+    {
+        /// <summary>
+        /// Message to string
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                return ArrayMethods.JoinList((List<string>)value, "\r\n");
+            }
+            catch (NullReferenceException)
+            {
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// String to message
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string[] delimiter = { "\r\n" };
+
+            try
+            {
+                return ((string)value).Split(delimiter, StringSplitOptions.None).ToList();
+            }
+            catch (NullReferenceException)
+            {
+                string[] emptyArr = { "" };
+                return emptyArr;
+            }
+        }
+    }
+}
