8780eb551d43f273b13ba98ae0ef84800fd1e1dd
Ryan Chang <rchang0369@gmail.com>
2020-05-06 15:30:40 -0400

    Added previewing of messages

--- RuinsOfAlbertrizal/Text/Message.cs
+++ RuinsOfAlbertrizal/Text/Message.cs
@@ -12,13 +12,14 @@ namespace RuinsOfAlbertrizal.Text
         public List<string> Lines { get; set; }
 
         public Message()
-        { }
+        {
+            Lines = new List<string>();
+        }
 
         /// <summary>
         /// Creates a new Text object that will make text available to diplay after conditions are met.
         /// </summary>
         /// <param name="lines">The message to display.</param>
-        /// <param name="pointCompletion">The point completion when the message will be fired at.</param>
         public Message(List<string> lines)
         {
             Lines = lines;
@@ -54,6 +55,27 @@ namespace RuinsOfAlbertrizal.Text
         public List<string> ToStringList()
         {
             return Lines;
+        }
+
+        /// <summary>
+        /// Gets a preview of the message
+        /// </summary>
+        /// <param name="characters">The numbers of charecters before the ...</param>
+        /// <returns></returns>
+        public string GetPreview(int characters)
+        {
+            if (Lines == null || Lines.Count < 1)
+                return "";
+            string str = Lines[0];
+            if (str == null)
+                return "";
+            else if (str.Count() <= characters)
+                return str;
+            else
+            {
+                str = str.Substring(0, characters) + "...";
+                return str;
+            }
         }
     }
 }
