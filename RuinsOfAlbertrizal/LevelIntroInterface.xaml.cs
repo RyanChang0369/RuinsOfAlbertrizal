2fb5c24b1a57ac5e53ce31eb969dc3f41fe198f0
Ryan Chang <rchang0369@gmail.com>
2020-05-11 10:09:43 -0400

    Added levelintrointeface

---
+++ RuinsOfAlbertrizal/LevelIntroInterface.xaml.cs
@@ -0,0 +1,57 @@
+﻿using RuinsOfAlbertrizal.XMLInterpreter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RuinsOfAlbertrizal
+{
+    /// <summary>
+    /// Interaction logic for IntroInterface.xaml
+    /// </summary>
+    public partial class LevelIntroInterface : Page
+    {
+        public LevelIntroInterface()
+        {
+            InitializeComponent();
+
+            DataContext = GameBase.CurrentGame.CurrentLevel;
+
+            if (!GameBase.CurrentGame.CurrentLevel.SeenIntroduction || true)
+            {
+                GameBase.CurrentGame.CurrentLevel.IntroMessage.InitializeControls(IntroText, NextBtn, SkipBtn);
+                GameBase.CurrentGame.CurrentLevel.IntroMessage.Display();
+
+                GameBase.CurrentGame.CurrentLevel.SeenIntroduction = true;
+                FileHandler.SaveCurrentMap();
+            }
+        }
+
+        private void NavAdventureInterface()
+        {
+            NavigationService.Navigate(new Uri("AdventureInterface.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void SkipBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavAdventureInterface();
+        }
+
+        private void NextBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (GameBase.CurrentGame.CurrentLevel.IntroMessage.NextBtnIsSkip())
+                NavAdventureInterface();
+        }
+    }
+}
