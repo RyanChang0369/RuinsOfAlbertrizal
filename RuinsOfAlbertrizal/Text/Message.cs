fc74f646544357d317490622146037e3becfc1ba
Ryan Chang <rchang0369@gmail.com>
2020-05-17 16:22:26 -0400

    Removed unnessiary usings

--- RuinsOfAlbertrizal/Text/Message.cs
+++ RuinsOfAlbertrizal/Text/Message.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Navigation;
 using System.Xml.Serialization;
 
 namespace RuinsOfAlbertrizal.Text
