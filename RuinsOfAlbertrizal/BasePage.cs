7bd6c56ee838332e210abc6a9c4aeac5465cf3a3
Ryan Chang <rchang0369@gmail.com>
2020-05-21 16:36:24 -0400

    Handles navigation

---
+++ RuinsOfAlbertrizal/BasePage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RuinsOfAlbertrizal
+{
+    public abstract class BasePage : Page
+    {
+        /// <summary>
+        /// Uses tags to navigate to correct editor page (relative uri).
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Navigate(object sender, RoutedEventArgs e)
+        {
+            Control ctrl = (Control)sender;
+            Navigate((string)ctrl.Tag);
+        }
+
+        public void Navigate(string location)
+        {
+            Navigate(new Uri(location, UriKind.RelativeOrAbsolute));
+        }
+
+        public void Navigate(Uri location)
+        {
+            NavigationService.Navigate(location);
+        }
+    }
+}
