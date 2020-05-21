9c424bf993adbad577abfa7065d6e8d82c48ca00
Ryan Chang <rchang0369@gmail.com>
2020-05-21 14:03:31 -0400

    Revamped enum displaying

--- RuinsOfAlbertrizal/Environment/Hazard.cs
+++ RuinsOfAlbertrizal/Environment/Hazard.cs
@@ -15,6 +15,45 @@ namespace RuinsOfAlbertrizal.Environment
 
         public List<Buff> Buffs { get; set; }
 
+        public enum DamageDirection
+        {
+            /// <summary>
+            /// No damage
+            /// </summary>
+            [Description("No damage")]
+            None,
+            /// <summary>
+            /// Damages on all surfaces, including the inside.
+            /// </summary>
+            [Description("Damages on all surfaces, including the inside.")]
+            All,
+            /// <summary>
+            /// Damages when character is inside this hazard
+            /// </summary>
+            [Description("Damages when character is inside this hazard")]
+            Inside,
+            /// <summary>
+            /// Damages when character is on top of this harzard.
+            /// </summary>
+            [Description("Damages when character is on top of this harzard.")]
+            Top,
+            /// <summary>
+            /// Damages when character is directly below this hazard.
+            /// </summary>
+            [Description("Damages when character is directly below this hazard.")]
+            Bottom,
+            /// <summary>
+            /// Damages when character is directly left of this hazard.
+            /// </summary>
+            [Description("Damages when character is directly left of this hazard.")]
+            Left,
+            /// <summary>
+            /// Damages when character is directly right of this hazard.
+            /// </summary>
+            [Description("Damages when character is directly right of this hazard.")]
+            Right
+        }
+
         public DamageDirection DirectionOfDamage { get; set; }
 
         public Hazard()
