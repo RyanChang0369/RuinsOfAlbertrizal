1a6a6adc1f27fc001d6880ed57793f30ea07af92
Ryan Chang <rchang0369@gmail.com>
2020-05-27 16:06:01 -0400

    Added active players

--- RuinsOfAlbertrizal/Environment/Map.cs
+++ RuinsOfAlbertrizal/Environment/Map.cs
@@ -49,6 +49,28 @@ namespace RuinsOfAlbertrizal.Environment
             }
         }
 
+        /// <summary>
+        /// The players that can attack. Also is the first 4 players in the Players list.
+        /// </summary>
+        [XmlIgnore]
+        public List<Player> ActivePlayers
+        {
+            get
+            {
+                List<Player> players = new List<Player>();
+
+                for (int i = 0; i < Players.Count; i++)
+                {
+                    if (i > 3)
+                        break;
+
+                    players.Add(Players[i]);
+                }
+
+                return players;
+            }
+        }
+
         public List<Boss> StoredBosses { get; set; }
 
         public List<Enemy> StoredEnemies { get; set; }
