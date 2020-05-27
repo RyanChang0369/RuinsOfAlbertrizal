23686a1e8f1830f1541c6bb8927002a497f92471
Ryan Chang <rchang0369@gmail.com>
2020-05-27 14:33:14 -0400

    Added deepclone method

--- RuinsOfAlbertrizal/MiscMethods.cs
+++ RuinsOfAlbertrizal/MiscMethods.cs
@@ -81,5 +81,17 @@ namespace RuinsOfAlbertrizal
 
             return "";
         }
+
+        public static T DeepClone<T>(this T obj)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(ms, obj);
+                ms.Position = 0;
+
+                return (T)formatter.Deserialize(ms);
+            }
+        }
     }
 }
