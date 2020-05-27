627fcabf7813fbf72a9dda179422c04895b7d08f
Ryan Chang <rchang0369@gmail.com>
2020-05-27 14:33:32 -0400

    Added advanced random events

---
+++ RuinsOfAlbertrizal/RandomEvent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuinsOfAlbertrizal
+{
+    public class RandomEvent : IComparable<RandomEvent>
+    {
+        /// <summary>
+        /// The larger the chance, the more likely the event will happen.
+        /// Chances range from 0.0 - 1.0. Chances with values of 1.0 are NOT GUAREENTEED to happen,
+        /// but chances with values of 0.0 are GUAREENTEED NOT to happen.
+        /// </summary>
+        public double Chance { get; set; }
+
+        public object Tag { get; set; }
+
+        /// <summary>
+        /// Creates a new random event.
+        /// </summary>
+        /// <param name="tag">The name of the event.</param>
+        /// <param name="chance">The larger the chance, the more likely the event will happen.
+        /// Chances range from 0.0 - 1.0. Chances with values of 1.0 are NOT GUAREENTEED to happen,
+        /// but chances with values of 0.0 are GUAREENTEED NOT to happen.</param>
+        public RandomEvent(object tag, double chance)
+        {
+            Tag = tag;
+            Chance = chance;
+        }
+
+        public int CompareTo(RandomEvent other)
+        {
+            return Chance.CompareTo(other.Chance);
+        }
+    }
+}
---
+++ RuinsOfAlbertrizal/RandomEventChooser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuinsOfAlbertrizal
+{
+    public class RandomEventChooser
+    {
+        /// <summary>
+        /// A list of chances with larger numbers being more likely to occur.
+        /// </summary>
+        public List<RandomEvent> RandomEvents { get; set; }
+
+        public RandomEventChooser(List<RandomEvent> randomEvents)
+        {
+            RandomEvents = randomEvents;
+        }
+
+        /// <summary>
+        /// Gets the selected random event
+        /// </summary>
+        /// <returns></returns>
+        public RandomEvent GetSelectedRandomEvent()
+        {
+            List<RandomEvent> selectedEvents = new List<RandomEvent>();
+
+            foreach (RandomEvent randomEvent in RandomEvents)
+            {
+                double fateSelector = RNG.GetRandomDouble();
+                if (randomEvent.Chance > fateSelector)
+                    selectedEvents.Add(randomEvent);
+            }
+
+            if (selectedEvents.Count < 1)
+                return GetSelectedRandomEvent();
+            else
+            {
+                int fateSelector2 = RNG.GetRandomInteger(selectedEvents.Count);
+                return selectedEvents[fateSelector2];
+            }
+        }
+
+        public object GetSelectedTag()
+        {
+            return GetSelectedRandomEvent().Tag;
+        }
+    }
+}
