c8a85c6b431fe1f47a068f62e52ce57d106e01fe
Ryan Chang <rchang0369@gmail.com>
2020-06-01 13:59:23 -0400

    Did a thing

---
+++ RuinsOfAlbertrizal/Basewindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace RuinsOfAlbertrizal
+{
+    public abstract class BaseWindow : Window
+    {
+        public void Close(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        public void Animate(string storyboardName, FrameworkElement element)
+        {
+            Storyboard storyboard = (Storyboard)Resources[storyboardName];
+            storyboard.Begin(element);
+        }
+    }
+}
