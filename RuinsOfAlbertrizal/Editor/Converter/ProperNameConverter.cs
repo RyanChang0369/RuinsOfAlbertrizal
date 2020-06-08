f1d46617c7f41072a0617c28f11a501078df2e9c
Ryan Chang <rchang0369@gmail.com>
2020-06-08 11:48:06 -0400

    Added ProperNameConverter

---
+++ RuinsOfAlbertrizal/Editor/Converter/ProperNameConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace RuinsOfAlbertrizal.Editor.Converter
+{
+    public class ProperNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                return ((ObjectOfAlbertrizal)value).DisplayName;
+            }
+            catch (InvalidCastException)
+            {
+                return ((List<ObjectOfAlbertrizal>)value)[((List<ObjectOfAlbertrizal>)value).Count - 1].DisplayName;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
