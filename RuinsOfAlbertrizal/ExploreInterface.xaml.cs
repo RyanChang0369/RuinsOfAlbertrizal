6646f19a466863c3596d1f80e6bd1c780894ac8f
Ryan Chang <rchang0369@gmail.com>
2020-06-12 09:46:18 -0400

    Simplified DoFindItem

--- RuinsOfAlbertrizal/ExploreInterface.xaml.cs
+++ RuinsOfAlbertrizal/ExploreInterface.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using RuinsOfAlbertrizal.Characters;
 using RuinsOfAlbertrizal.Environment;
 using RuinsOfAlbertrizal.Items;
+using RuinsOfAlbertrizal.XMLInterpreter;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -97,40 +98,52 @@ namespace RuinsOfAlbertrizal
 
         private void DoFindItem()
         {
-            //Check if these items affect the stored items.
-            List<RandomEvent> randomEventsItems = new List<RandomEvent>();
-            foreach (Item item in GameBase.CurrentGame.StoredItems)
+            //Check if stored items are affected
+            List<RandomEvent> randomEvents = new List<RandomEvent>();
+            foreach (Item item in GameBase.CurrentGame.AllItems)
             {
-                randomEventsItems.Add(new RandomEvent(item, item.DropChance));
+                randomEvents.Add(new RandomEvent(item, item.DropChance));
             }
-            Item selectedItem = (Item)new RandomEventChooser(randomEventsItems).GetSelectedTag();
+            Item selected = (Item)new RandomEventChooser(randomEvents).GetSelected();
 
-            List<RandomEvent> randomEventsEquiptments = new List<RandomEvent>();
-            foreach (Equiptment equiptment in GameBase.CurrentGame.StoredEquiptments)
-            {
-                randomEventsEquiptments.Add(new RandomEvent(equiptment, equiptment.DropChance));
-            }
-            Equiptment selectedEquiptment = (Equiptment)new RandomEventChooser(randomEventsEquiptments).GetSelectedTag();
+            GameBase.CurrentGame.PlayerObtainObject(selected);
 
-            List<RandomEvent> randomEventsConsumables = new List<RandomEvent>();
-            foreach (Consumable consumable in GameBase.CurrentGame.StoredConsumables)
-            {
-                randomEventsConsumables.Add(new RandomEvent(consumable, consumable.DropChance));
-            }
-            Consumable selectedConsumable = (Consumable)new RandomEventChooser(randomEventsConsumables).GetSelectedTag();
+            ////Check if these items affect the stored items.
+            //List<RandomEvent> randomEventsItems = new List<RandomEvent>();
+            //foreach (Item item in GameBase.CurrentGame.StoredItems)
+            //{
+            //    randomEventsItems.Add(new RandomEvent(item, item.DropChance));
+            //}
+            //Item selectedItem = (Item)new RandomEventChooser(randomEventsItems).GetSelected();
 
-            switch (RNG.GetRandomInteger(3))
-            {
-                case 0:
-                    SelectedPlayer.ObtainItem(selectedItem);
-                    break;
-                case 1:
-                    SelectedPlayer.ObtainEquiptment(selectedEquiptment);
-                    break;
-                case 2:
-                    SelectedPlayer.ObtainConsumable(selectedConsumable);
-                    break;
-            }
+            //List<RandomEvent> randomEventsEquiptments = new List<RandomEvent>();
+            //foreach (Equiptment equiptment in GameBase.CurrentGame.StoredEquiptments)
+            //{
+            //    randomEventsEquiptments.Add(new RandomEvent(equiptment, equiptment.DropChance));
+            //}
+            //Equiptment selectedEquiptment = (Equiptment)new RandomEventChooser(randomEventsEquiptments).GetSelected();
+
+            //List<RandomEvent> randomEventsConsumables = new List<RandomEvent>();
+            //foreach (Consumable consumable in GameBase.CurrentGame.StoredConsumables)
+            //{
+            //    randomEventsConsumables.Add(new RandomEvent(consumable, consumable.DropChance));
+            //}
+            //Consumable selectedConsumable = (Consumable)new RandomEventChooser(randomEventsConsumables).GetSelected();
+
+            //switch (RNG.GetRandomInteger(3))
+            //{
+            //    case 0:
+            //        GameBase.CurrentGame.ObtainItem(selectedItem);
+            //        break;
+            //    case 1:
+            //        GameBase.CurrentGame.ObtainEquiptment(selectedEquiptment);
+            //        break;
+            //    case 2:
+            //        GameBase.CurrentGame.ObtainConsumable(selectedConsumable);
+            //        break;
+            //}
+
+            FileHandler.SaveCurrentMap();
         }
 
         private void DoRandomEncounter()
