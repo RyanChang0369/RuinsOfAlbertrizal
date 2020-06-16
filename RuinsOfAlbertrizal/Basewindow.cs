39b3d40b789cb611afa662f149e54927588a12ad
Ryan Chang <rchang0369@gmail.com>
2020-06-16 15:39:36 -0400

    Imported from BasePage

--- RuinsOfAlbertrizal/Basewindow.cs
+++ RuinsOfAlbertrizal/Basewindow.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using RuinsOfAlbertrizal.XMLInterpreter;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media.Animation;
 
 namespace RuinsOfAlbertrizal
@@ -15,10 +17,31 @@ namespace RuinsOfAlbertrizal
             Close();
         }
 
+        public void SaveCurrentGame(object sender, RoutedEventArgs e)
+        {
+            FileHandler.SaveCurrentMap();
+            MessageBox.Show("Game Saved!");
+        }
+
+        public void SaveStaticGame(object sender, RoutedEventArgs e)
+        {
+            FileHandler.SaveStaticMap();
+            MessageBox.Show("Game Saved!");
+        }
+
         public void Animate(string storyboardName, FrameworkElement element)
         {
             Storyboard storyboard = (Storyboard)Resources[storyboardName];
             storyboard.Begin(element);
+        }
+
+        /// <summary>
+        /// Forces the ItemsSource property of the listbox to update
+        /// </summary>
+        public static void ForceListBoxUpdate(ListBox listBox)
+        {
+            listBox.GetBindingExpression(ListBox.ItemsSourceProperty).UpdateTarget();
+            listBox.Items.Refresh();
         }
     }
 }
