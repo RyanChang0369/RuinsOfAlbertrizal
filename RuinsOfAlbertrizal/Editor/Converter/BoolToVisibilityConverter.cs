d59fabcfecca9522af788ce427670b3a50cd292b
Ryan Chang <rchang0369@gmail.com>
2020-06-16 15:38:24 -0400

    Added class to convert bool to visibility

---
+++ RuinsOfAlbertrizal/Editor/Converter/BoolToVisibilityConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RuinsOfAlbertrizal.Editor.Converter
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return Visibility.Visible;
+            else
+                return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Visibility visibility = (Visibility)value;
+            switch(visibility)
+            {
+                case Visibility.Visible:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
