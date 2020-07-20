41412b87fe0a6bff5d684e3ee5060fdfe4e34208
Ryan Chang <rchang0369@gmail.com>
2020-07-20 15:22:24 -0400

    Added exception

---
+++ RuinsOfAlbertrizal/Exceptions/DidNotMoveException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuinsOfAlbertrizal.Exceptions
+{
+    /// <summary>
+    /// Thrown if AI fails to move the enemy to a different location.
+    /// </summary>
+    public class DidNotMoveException : Exception
+    {
+        public DidNotMoveException() { }
+
+        public DidNotMoveException(string message) : base(message) { }
+
+        public DidNotMoveException(string message, Exception inner) : base(message, inner) { }
+    }
+}
