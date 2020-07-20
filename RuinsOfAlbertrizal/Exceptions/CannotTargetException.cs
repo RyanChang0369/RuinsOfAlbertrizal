b9606f8eef4fcd22f7d8d6b68d3d623a340522da
Ryan Chang <rchang0369@gmail.com>
2020-07-20 15:22:14 -0400

    Made public

--- RuinsOfAlbertrizal/Exceptions/CannotTargetException.cs
+++ RuinsOfAlbertrizal/Exceptions/CannotTargetException.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RuinsOfAlbertrizal.Exceptions
 {
-    class CannotTargetException : Exception
+    public class CannotTargetException : Exception
     {
         public CannotTargetException() { }
 
