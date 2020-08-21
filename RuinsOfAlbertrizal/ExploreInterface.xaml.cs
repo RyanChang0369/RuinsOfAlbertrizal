f46778e423db1e4f06648296622a1ce0e779250a
Ryan Chang <rchang0369@gmail.com>
2020-08-21 11:16:13 -0400

    Removed uneeded code

--- RuinsOfAlbertrizal/ExploreInterface.xaml.cs
+++ RuinsOfAlbertrizal/ExploreInterface.xaml.cs
@@ -93,41 +93,6 @@ namespace RuinsOfAlbertrizal
 
             GameBase.CurrentGame.PlayerObtainObject(selected);
 
-            ////Check if these items affect the stored items.
-            //List<RandomEvent> randomEventsItems = new List<RandomEvent>();
-            //foreach (Item item in GameBase.CurrentGame.StoredItems)
-            //{
-            //    randomEventsItems.Add(new RandomEvent(item, item.DropChance));
-            //}
-            //Item selectedItem = (Item)new RandomEventChooser(randomEventsItems).GetSelected();
-
-            //List<RandomEvent> randomEventsEquiptments = new List<RandomEvent>();
-            //foreach (Equiptment equiptment in GameBase.CurrentGame.StoredEquiptments)
-            //{
-            //    randomEventsEquiptments.Add(new RandomEvent(equiptment, equiptment.DropChance));
-            //}
-            //Equiptment selectedEquiptment = (Equiptment)new RandomEventChooser(randomEventsEquiptments).GetSelected();
-
-            //List<RandomEvent> randomEventsConsumables = new List<RandomEvent>();
-            //foreach (Consumable consumable in GameBase.CurrentGame.StoredConsumables)
-            //{
-            //    randomEventsConsumables.Add(new RandomEvent(consumable, consumable.DropChance));
-            //}
-            //Consumable selectedConsumable = (Consumable)new RandomEventChooser(randomEventsConsumables).GetSelected();
-
-            //switch (RNG.GetRandomInteger(3))
-            //{
-            //    case 0:
-            //        GameBase.CurrentGame.ObtainItem(selectedItem);
-            //        break;
-            //    case 1:
-            //        GameBase.CurrentGame.ObtainEquiptment(selectedEquiptment);
-            //        break;
-            //    case 2:
-            //        GameBase.CurrentGame.ObtainConsumable(selectedConsumable);
-            //        break;
-            //}
-
             FileHandler.SaveCurrentMap();
         }
 
